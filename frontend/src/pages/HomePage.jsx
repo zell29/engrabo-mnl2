8c9ef8da27fb529c8dd85062aabe1b06189e0c28
Raymond Dave Guibone <monmonguibone@gmail.com>
2024-03-06 15:08:38 +0800

    creating and adding Category components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Layout/Header';
-import Slider from '../components/Route/Hero/Slider';
+import Slider from '../components/Route/Slider/Slider';
+import Categories from '../components/Route/Categories/Categories';
 const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
       <Slider />
+      <Categories />
     </div>
   );
 };
