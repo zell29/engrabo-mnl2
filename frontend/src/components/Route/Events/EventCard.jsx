224e96ecd2fe70a8d8971b8eab55294c3217f7c3
Monmon <personaldev1009@gmail.com>
2024-04-24 16:52:17 +0800

    new

--- frontend/src/components/Route/Events/EventCard.jsx
+++ frontend/src/components/Route/Events/EventCard.jsx
@@ -3,24 +3,27 @@ import styles from '../../../styles/style';
 import CountDown from './CountDown';
 import { backend_url } from '../../../server';
 
-const EventCard = ({ active, data }) => {
-  console.log(data);
+const EventCard = ({ data }) => {
+  // Add a guard clause to check if data is available
+  if (!data || !data.images || data.images.length === 0) {
+    console.log('Data or images not provided or empty in EventCard');
+    return <div>Loading event data...</div>; // Render a fallback or a loading indicator
+  }
+
   return (
     <div
-      className={`flex flex-col lg:flex-row w-full bg-white rounded-lg ${
-        active ? 'unset' : 'mb-12'
-      } p-2 justify-center items-center`}
+      className={`flex flex-col lg:flex-row w-full bg-white rounded-lg mb-12 p-2 justify-center items-center`}
     >
       <div className="w-[400px] h-[420px] 800px:pt-3 800px:pb-3 pb-0 pt-3 lg:w-1/2 800px:pr-[140px] pr-0 flex justify-center">
-        <img src={`${backend_url}/${data.images[0]}`} alt="" />
+        <img
+          src={`${backend_url}/${data.images[0]}`}
+          alt={data.name || 'Event'}
+        />
       </div>
       <div className="w-full lg:w-1/2 flex flex-col justify-center items-start p-4">
-        {/* Title of Product */}
         <h2 className={`${styles.productTitle} text-center lg:text-left`}>
           {data.name}
         </h2>
-
-        {/* Description of Product */}
         <p className="text-justify text-[#534723]">{data.description}</p>
         <div className="flex py-2 justify-between w-full">
           {/* Price of Product */}
